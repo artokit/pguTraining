df42557baa0066fd32700910c34697e3d097efc1
artokit <kdsa3102@gmail.com>
2024-03-06 18:40:07 +0300

    Add migrations

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -1,6 +1,26 @@
+using System.Reflection;
+using FluentMigrator.Runner;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services
+    .AddFluentMigratorCore().ConfigureRunner(rb =>
+        rb.AddPostgres()
+            .WithGlobalConnectionString(connectionString)
+            .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole())
+    .BuildServiceProvider(false);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using var serviceProvider = app.Services.CreateScope();
+var services = serviceProvider.ServiceProvider;
+var runner = services.GetRequiredService<IMigrationRunner>();
+runner.MigrateUp();
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
--- WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e333cf9e164f92433a109e55920fa77d51d84172")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
